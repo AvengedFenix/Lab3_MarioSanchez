2150ffbc6582c3fd26887e72d5d70af2f18e05de
Mario <Maslz980outlook.com>
2017-02-03 18:18:25 -0600

    ej 1 a medias

--- Lab3_MarioSanchez.cpp
+++ Lab3_MarioSanchez.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
+#include<vector>
 
 using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
+using std::vector;
 
 void menu(int);
 
@@ -24,9 +26,76 @@ int main(){
 void menu(int men){
 	switch(men){
 		case 1:{
-				
-		}
+			vector<string> vect;
+			int err =0;
+			while(err == 0){
+				cout<<"Ingrese una fecha YYYY/MM/DD"<<endl;
+				string fecha;
+				cin>>fecha;
+				string ano;
+				string mes;
+				string dia;
+				if(fecha.length()>8){
+					cout<<"No puede ingresar una fecha que no existe"<<endl;
+					err = 1;
+				} else{
+					for(int i = 0; i<4; i++){
+						if(i<4){
+							ano += fecha.at(i);
+						}else if(i>3 && i<6){
+							mes += fecha.at(i);
+						}else if(i>5 && i<8){
+							dia += fecha.at(i);
+						}
+					}
+					if(ano > "2017"){
+						cout<<"Ese año aun existe"<<endl;
+						err = 1;
+					}
+					if(mes > "12"){
+						cout<<"Ese mes no existe"<<endl;
+						err = 1;
+					}
+					if((mes == "01" || mes == "03" || mes == "05" || mes == "07" || mes == "08" || mes == "10" || mes == "12") && stoi(dia) > 31){
+						cout<<"Ese dia no existe"<<endl;
+						err = 1;
+					}else if((mes == "04" || mes == "06" || mes == "09" || mes == "11") && stoi(dia) > 30){
+						cout<<"Ese dia no existe"<<endl;
+						err = 1;
+					} else{
+						int bis = 0;
+						if(stoi(ano) % 4 == 0){
+							bis++;
+						}
+						if(stoi(ano) % 100 == 0){
+							bis++;
+						}
+						if(stoi(ano) % 400 == 0){
+							bis++;
+						}
+						if(bis == 3){
+							if(mes == "02" && stoi(dia) >29){
+								cout<<"Ese dia no existe"<<endl;
+								err = 1;
+							}
+						}else{
+							if(mes == "02" && stoi(dia) > 28){
+								cout<<"Ese dia no existe"<<endl;
+								err = 1;
+							}
+						}
+					}
+				cout<<"Listando..."<<endl;
+				vect.push_back(fecha);
+				for(int i = 0; i < vect.size(); i++){
+					cout<<i<<". "<<vect[i]<<endl;
+				}
+				}
+			}
+			break;
+		} 
 		case 2:{
+		
 			string sign;
 			string c;
 			cout<<"Ingrese un caracter"<<endl;
@@ -95,6 +164,8 @@ void menu(int men){
 					cout<<"Los signos no son balanceados"<<endl;
 				}
 			}
+			break;
+
 		}
 	}
 }
